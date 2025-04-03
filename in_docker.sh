2b5239b4408b1463538d5abea1ca217014940e90
Christian Köstlin <christian.koestlin@gmail.com>
2025-04-04 00:24:57 +0200

    Preserve docker build artifacts

---
+++ in_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+project=`basename $(pwd)`
+
+INTERACTIVE=
+if [ -z "$@" ]
+then
+       INTERACTIVE="--interactive --tty"
+fi
+
+podman run --rm $INTERACTIVE -v$(pwd)/..:/ws -v$HOME/Sync/configs/sesame:/root/.config/sesame -v$HOME/Sync/configs/age:/root/.config/age -v$(pwd)/linux-out:/ws/19-sesame/out localhost/ldc "$@"
